95a971947a0ad6c40e9e601f554a0d7159b823d2
Mukku <mukkuraj0499@gmail.com>
2023-10-12 17:23:34 +0530

    upate code

--- 01-Basics/01-variables.js
+++ 01-Basics/01-variables.js
@@ -1,44 +1,60 @@
-console.log("Pushpendra Kumar")
-// Timming 32:40:00
-const accountId =144553; 
+// console.log("Pushpendra Kumar")
+// // Timming 32:40:00
+// const accountId =144553; 
 
 
-let accountEmail="mukkuverma.com"
-var accountPassword ="12345"
-accountCity ="Jaipur"    // do not declare like as without variables
+// let accountEmail="mukkuverma.com"
+// var accountPassword ="12345"
+// accountCity ="Jaipur"    // do not declare like as without variables
 
-let accountState;  //// show in result is Undefined
-// let accountState="";
+// let accountState;  //// show in result is Undefined
+// // let accountState="";
 
-/* +++++++++++++++++++++ notes ++++++++++++++++++
-1. var variables can be updated and re-declared within its scope; 
-2. let variables can be updated but not re-declared;
-3.  const variables can neither be updated nor re-declared
+// /* +++++++++++++++++++++ notes ++++++++++++++++++
+// 1. var variables can be updated and re-declared within its scope; 
+// 2. let variables can be updated but not re-declared;
+// 3.  const variables can neither be updated nor re-declared
 
 
- */
+//  */
 
-/*  1. To declare variables we can use let and var use for variables.
-  2.To declare constant we can use const
-*/
+// /*  1. To declare variables we can use let and var use for variables.
+//   2.To declare constant we can use const
+// */
 
-// accountId =13; // not allowed
+// // accountId =13; // not allowed
 
-/*
-1. Prefer not to use var 
-beacause of issue in block scope and functional scope
-2. we always use let in Javascript
- */
+// /*
+// 1. Prefer not to use var 
+// beacause of issue in block scope and functional scope
+// 2. we always use let in Javascript
+//  */
 
-//
+// //
  
-//accountId=2     ************ not allowed
+// //accountId=2     ************ not allowed
 
-accountEmail="puspa.com"
-accountPassword="121212"
-accountCity ="Bangaluru"
+// accountEmail="puspa.com"
+// accountPassword="121212"
+// accountCity ="Bangaluru"
 
 
 
-console.table([accountId,accountEmail,accountPassword,
-accountCity,accountState])
+// console.table([accountId,accountEmail,accountPassword,
+// accountCity,accountState])
+
+
+console.log("Pushpendra Kumar")
+const acccountId =12332;
+var accountEmail ="Mukkuraj121212";
+let accountPassword ="Kumar";
+
+
+const accountId =8776;
+var accountEmail ="mukku76536535";
+accountPassword="34349kumar";
+// acocuntState =''; // ''
+accountState ="radhe"
+
+
+console.table([acccountId,accountEmail,accountPassword,accountState])
--- 01-Basics/Start.js
+++ 01-Basics/Start.js
@@ -1,2 +1,3 @@
 console.log('Pushpendra Kumar') 
 console.log('How are you my coder family')
+//Timming ==> 3:10:00 
