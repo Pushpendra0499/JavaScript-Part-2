57658a6cc7459bd0996b7db4f0ea08ae86f55638
Mukku <mukkuraj0499@gmail.com>
2023-10-16 21:26:29 +0530

    update code

--- 03-Basics/01-functions.js
+++ 03-Basics/01-functions.js
@@ -16,21 +16,21 @@
 // console.log("Hello")
 // }
 
-// sayMyName // this is no shown error
+// // sayMyName // this is no shown error
 // sayMyName()
 
 
 
-function addTwoNumbers(num1,num2){
-    console.log(num1 + num2 );
-}
+// function addTwoNumbers(num1,num2){
+//     console.log(num1 + num2 );
+// }
 
 
-addTwoNumbers()  //NaN
+// addTwoNumbers()  //NaN
 // addTwoNumbers(2,"4") //24
-addTwoNumbers(3,4) //7
-// addTwoNumbers(2,a) //a is not defined(Error)
-addTwoNumbers(2,null) //2
+// addTwoNumbers(3,4) //7
+// // addTwoNumbers(2,a) //a is not defined(Error)
+// addTwoNumbers(2,null) //2
 
 // addTwoNumbers(2,"a")  // 2a
 // addTwoNumbers("a","b") //ab
@@ -44,23 +44,22 @@ function addTwoNumbers(num1,num2){
     return num1+num2; // yhis is inside the scope
 }
 //  Timming 4:44:00
-const result =addTwoNumbers(3,5) 
+// const result =addTwoNumbers(3,5) 
 //   console.log("Result :",result) //undefined how to go undefined value
 
-function loginUserrMessage(username){ // <== loginUserrMessage(username ="sam")
-    if(username===undefined) {  // <== we can use also this method if(!username)
-        console.log("Please enter a username")
+// function loginUserrMessage(username){ // <== loginUserrMessage(username ="sam")
+//     if(username===undefined) {  // <== we can use also this method if(!username)
+//         console.log("Please enter a username")
 
-        // ("") empty string will be taken like false value
-        // return 
-        // output will be undefined // if here we use return value than code does not exist in the next itration
+//         // ("") empty string will be taken like false value
+//         // return 
+//         // output will be undefined // if here we use return value than code does not exist in the next itration
 
-    }
-    return `${username} just loggeg in`
-}
+//     }
+//     return `${username} just loggeg in`
+// } 
 
 // loginUserrMessage("pushpendra")// this value will not print in the output
-
 // console.log(loginUserrMessage("pushpendra")); //  pushpendra just loggeg in   string print in the output
 // console.log(loginUserrMessage("")); // just loggeg in
 // console.log(loginUserrMessage());  //undefined just loggeg in
@@ -69,12 +68,15 @@ function loginUserrMessage(username){ // <== loginUserrMessage(username ="sam")
 //   ... this is also known as spread operator and rest operator this is depend upon use of case
 
 
-function calculateCarPrice(val1, val2, ...num1){
-return num1
-}
+// function calculateCarPrice(val1, val2, ...num1)
+// function calculateCarPrice(val1, ...num1) // 4:58:00
+// {
+// return num1;
+// }
+// console.log(calculateCarPrice(220,400,500,1000));  //[ 220, 400, 500 ,1000 ]
 
-console.log(calculateCarPrice(220,400,500,1000));  //[ 220, 400, 500 ,1000 ]
 
+// Pass the object to the function
 const user ={
     username:"pushpenra",
     price:199
@@ -84,10 +86,9 @@ function handleObject(anyobject){
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 // but pproblem is comming if we use of prices in place oof price
 }
-
  handleObject(user) //Username is pushpenra and price is 199
 
-// // good method
+// // good method we can update the username and price
 
 handleObject({
     username:"Pushpa",
@@ -95,14 +96,11 @@ handleObject({
 }) // output  ==>  Username is Pushpa and price is 399
 
 const myNewArray=[200,400,100,600]
-
 function returnSecondValue(getArray){
     return getArray[3]
 }
-
-
 console.log(returnSecondValue(myNewArray)); //600
-console.log(returnSecondValue([200,400,500,1000])); // result will we get 1000
+// console.log(returnSecondValue([200,400,500,1000])); // result will we get 1000
 
 
 
